72cccd6abdb52156f902ec363eed1950350d6039
aymen <aymen.sellaouti@gmail.com>
2019-04-16 15:37:27 +0100

    add Todo

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -3,8 +3,8 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends Controller
 {
@@ -24,5 +24,35 @@ class TodoController extends Controller
         }
         return $this->render('@App/Todo/index.html.twig');
     }
+
+    /**
+     * @param $titre
+     * @param $contenu
+     * @Route("/todo/add/{titre}/{contenu}")
+     */
+    public function addAction(Request $request, $titre, $contenu) {
+        // vérifier lexistance de la session
+        $session = $request->getSession();
+        if(!$session->has('mesTodos')) {
+            // !ok
+            // message erreur
+            $session->getFlashBag()->add('error', "La liste des todos est innexistante");
+        } else {
+            //si ok
+            // Vérifier que le todo n'existe pas déja
+            $mesTodos = $session->get('mesTodos');
+            if (isset($mesTodos[$titre])) {
+                // si oui
+                // Message erreur
+                $session->getFlashBag()->add('error', "Le todo ${titre} existe déjà");
+            } else {
+                $mesTodos[$titre] = $contenu;
+                $session->set('mesTodos', $mesTodos);
+                $session->getFlashBag()->add('success', "Le todo ${titre} a été ajouté avec succées");
+            }
+        }
+        return $this->forward('AppBundle:Todo:index');
+    }
+
 
 }
